f71e193a68840f9683d85af6a00d27381b6aeb40
TileGena <tn.asankulov@gmail.com>
2023-11-03 16:44:44 +0600

    Summary: Create new Employee-app component,module, make custom mat-toolbar
    Test Plan: * run client and server,
    * run unit tests `npm run test`

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeAppModule } from './employee-app/employee-app.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    EmployeeAppModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/employee-app/employee-app-root/employee-app-root.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-employee-app-root',
+  templateUrl: './employee-app-root.component.html',
+  styleUrls: ['./employee-app-root.component.scss']
+})
+export class EmployeeAppRootComponent {
+
+}
